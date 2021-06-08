8e2926ca6a31c14e9fa8a31b2e2f5d1afe10d9ed
Gabriela Silva <gabisantos.enf@gmail.com>
2021-06-08 11:34:44 +0200

    scroll up & slideshow

--- js/myScript.js
+++ js/myScript.js
@@ -8,7 +8,7 @@ function myFunction() {
   }
 }
 
-//Slieshow
+//Change Slieshow
 const slideshowImages = document.querySelectorAll(".intro .slideshow-img");
 
 const time = 5000;
@@ -18,7 +18,45 @@ slideshowImages[currentImageCounter].style.display = "block";
 
 setInterval(nextImage, time);
   function nextImage() {
-  slideshowImages[currentImageCounter ].style.display = "none";
-  currentImageCounter  = (currentImageCounter  + 1)  % slideshowImages.length;
-   slideshowImages [currentImageCounter ].style.display = "block";
+    slideshowImages[currentImageCounter].style.display = "none";
+    currentImageCounter  = (currentImageCounter  + 1)  % slideshowImages.length;
+    slideshowImages [currentImageCounter].style.display = "block";
+  }
+
+const slideshowText = document.querySelectorAll(".intro .slideshow-txt");
+
+// Change Text
+let currentTextCounter = 0;
+slideshowText[currentTextCounter].style.display = "block";
+
+setInterval(nextText, time);
+  function nextText () {
+    slideshowText[currentTextCounter].style.display = "none";
+    currentTextCounter  = (currentTextCounter + 1)  % slideshowImages.length;
+    slideshowText [currentTextCounter].style.display = "block";
+  }
+
+//Scroll to the top
+
+topbutton = document.getElementById("myBtn");
+
+window.onscroll = () => {scrollFunc()};
+
+function scrollFunc() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
 }
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+
+
+
+
+
